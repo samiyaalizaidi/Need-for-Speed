b6b71e02c90a6b4cc9364668bc2e1f4efad7e1f7
Samiya Ali Zaidi <sa07171@st.habib.edu.pk>
2022-11-02 21:36:19 +0500

    removed param for ctor & changed attack()'s to str

--- performance.hpp
+++ performance.hpp
@@ -1,39 +1,45 @@
+#include<iostream>
+using namespace std;
 
 /* 
 this class is for the health and lives of our spaceship.
-it only has two attributes:
-    1. minutes
-    2. seconds
+it only has two attributes, both are private because user should not change them on their own:
+    1. health
+    2. lives
 Class functions:
-    1. adjust()
-        the adjust function will adjust the value of the seconds so that they don't exceed 59.
+    1. attack()
+        the attack function will adjust the values of the lives and health
+        after an attack - either laser or canon.
 */
 
 class performance{
     int health;
     int lives;
-    performance(int,int){
-        health=100;
-        lives=2;
+    performance(){
+        // constructor, we dont need inputs because initial values are fixed.
+        health = 100;
+        lives = 2;
     }
-    void attack(//type_of_attack att){ //type_of_attack is a class with 2 types of attack
+    void attack(string att){ 
+        //type_of_attack is a class with 2 types of attack
         //att is object of type of attack, will be given as argument
-        while( lives >=1){
-            if(health =0 && lives>1){
-            if (att ="canon"){ //ASk {
-                health = health - 20;
+        while(lives >= 1){
+            if(health == 0 && lives > 1){
+                if (att == "canon"){ 
+                    //ASk-> sidra wanted to confirm smth? 
+                    health = health - 20;
+                }
+                else{
+                    health = health  - 10;
+                }
             }
-            else{
-                health = health  - 10;
-            }
-        }
-            else if (health=0 && lives <=1 ){
+            else if (health == 0 && lives <= 1 ){
 
                ///COnfusion, ask out about sequence of conditons,
                // health =0, lives =2 then lives-1
                //lives - 1 each time health = 0 until lives =0
 
-        }
+            }
         }
     }
 };
--- position.hpp
+++ position.hpp
@@ -1,4 +1,4 @@
 class position{
-    int x; //x cordinte of spaceship on screen
+    int x; //x coordinte of spaceship on screen
     int y; //y cordinates of spaceship on screen
 };
