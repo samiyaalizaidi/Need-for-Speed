8e61eb040b4ae91398425d04a03910d0f5468c2b
Samiya Ali Zaidi <sa07171@st.habib.edu.pk>
2022-11-02 21:46:26 +0500

    ctor and attack should be public.

--- performance.hpp
+++ performance.hpp
@@ -13,33 +13,35 @@ Class functions:
 */
 
 class performance{
-    int health;
-    int lives;
-    performance(){
-        // constructor, we dont need inputs because initial values are fixed.
-        health = 100;
-        lives = 2;
-    }
-    void attack(string att){ 
-        //type_of_attack is a class with 2 types of attack
-        //att is object of type of attack, will be given as argument
-        while(lives >= 1){
-            if(health == 0 && lives > 1){
-                if (att == "canon"){ 
-                    //ASk-> sidra wanted to confirm smth? 
-                    health = health - 20;
+    private:
+        int health;
+        int lives;
+    public:
+        performance(){
+            // constructor, we dont need inputs because initial values are fixed.
+            health = 100;
+            lives = 2;
+        }
+        void attack(string att){ 
+            //type_of_attack is a class with 2 types of attack
+            //att is object of type of attack, will be given as argument
+            while(lives >= 1){
+                if(health == 0 && lives > 1){
+                    if (att == "canon"){ 
+                        //ASk-> sidra wanted to confirm smth? 
+                        health = health - 20;
+                    }
+                    else{
+                        health = health  - 10;
+                    }
                 }
-                else{
-                    health = health  - 10;
+                else if (health == 0 && lives <= 1 ){
+
+                ///COnfusion, ask out about sequence of conditons,
+                // health =0, lives =2 then lives-1
+                //lives - 1 each time health = 0 until lives =0
+
                 }
-            }
-            else if (health == 0 && lives <= 1 ){
-
-               ///COnfusion, ask out about sequence of conditons,
-               // health =0, lives =2 then lives-1
-               //lives - 1 each time health = 0 until lives =0
-
             }
         }
-    }
 };
