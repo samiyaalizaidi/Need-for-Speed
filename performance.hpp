e0c9d58689e47d34003265b90d878aa674b6a537
sidraAamir <98018667+sidraAamir@users.noreply.github.com>
2022-11-03 13:52:35 +0500

    check this

--- performance.hpp
+++ performance.hpp
@@ -22,26 +22,44 @@ class performance{
             health = 100;
             lives = 2;
         }
-        void attack(string att){ 
+        //void attack(string att){ 
             //type_of_attack is a class with 2 types of attack
             //att is object of type of attack, will be given as argument
-            while(lives >= 1){
-                if(health == 0 && lives > 1){
-                    if (att == "canon"){ 
-                        //ASk-> sidra wanted to confirm smth? 
-                        health = health - 20;
+        bool alive = true ;
+        void health_deter(){
+            if (lives == 2){
+                if (health>0){
+                    if (attack = canon){
+                        health -=20;
                     }
                     else{
-                        health = health  - 10;
+                        health -=10;
                     }
                 }
-                else if (health == 0 && lives <= 1 ){
-
-                ///COnfusion, ask out about sequence of conditons,
-                // health =0, lives =2 then lives-1
-                //lives - 1 each time health = 0 until lives =0
-
+                else if(health=0){
+                    lives -- ;
+                    if (health>0){
+                        if (attack = canon){
+                            health -=20;
+                        }
+                        else{
+                            health -=10;
+                        }
+                    }     
                 }
             }
-        }
+            else if(lives ==1){
+                if (health>0){
+                    if (attack = canon){
+                        health -=20;
+                    }
+                    else{
+                        health -=10;
+                    }
+                }
+                else if(health = 0){
+                    lives--;
+                    alive = false;
+                }
+            }
 };
