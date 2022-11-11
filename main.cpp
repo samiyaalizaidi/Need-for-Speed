6923dc385ac990f5f432d1e61a8cd83f9b670f48
JaveriaAzfar <ja07622@st.habib.edu.pk>
2022-11-11 15:41:26 +0500

    cpp properties sahi ki hain

--- main.cpp
+++ main.cpp
@@ -1,111 +1,111 @@
-#include <SDL.h>
-#include <SDL_image.h>
-// #include <SDL_mixer.h>
-#include <stdio.h>
-#include "performance.hpp"
-#include "levels.hpp"
+// #include <SDL.h>
+// #include <SDL_image.h>
+// // #include <SDL_mixer.h>
+// #include <stdio.h>
+// #include "performance.hpp"
+// #include "levels.hpp"
 
 
-//Screen dimension constants
-const int SCREEN_WIDTH = 800;
-const int SCREEN_HEIGHT = 600;
-// SDL Functions from hw1
+// //Screen dimension constants
+// const int SCREEN_WIDTH = 800;
+// const int SCREEN_HEIGHT = 600;
+// // SDL Functions from hw1
 
-bool init()
-{
-	//Initialization flag
-	bool success = true;
+// bool init()
+// {
+// 	//Initialization flag
+// 	bool success = true;
 
-	//Initialize SDL
-	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
-	{
-		printf( "SDL could not initialize! SDL Error: %s\n", SDL_GetError() );
-		success = false;
-	}
-	else
-	{
-		//Set texture filtering to linear
-		if( !SDL_SetHint( SDL_HINT_RENDER_SCALE_QUALITY, "1" ) )
-		{
-			printf( "Warning: Linear texture filtering not enabled!" );
-		}
+// 	//Initialize SDL
+// 	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+// 	{
+// 		printf( "SDL could not initialize! SDL Error: %s\n", SDL_GetError() );
+// 		success = false;
+// 	}
+// 	else
+// 	{
+// 		//Set texture filtering to linear
+// 		if( !SDL_SetHint( SDL_HINT_RENDER_SCALE_QUALITY, "1" ) )
+// 		{
+// 			printf( "Warning: Linear texture filtering not enabled!" );
+// 		}
 
-		//Create window
-		gWindow = SDL_CreateWindow( "Legend of Seeplusia!!", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-		if( gWindow == NULL )
-		{
-			printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
-			success = false;
-		}
-		else
-		{
-			//Create renderer for window
-			gRenderer = SDL_CreateRenderer( gWindow, -1, SDL_RENDERER_ACCELERATED );
-			if( gRenderer == NULL )
-			{
-				printf( "Renderer could not be created! SDL Error: %s\n", SDL_GetError() );
-				success = false;
-			}
-			else
-			{
-				//Initialize renderer color
-				SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
+// 		//Create window
+// 		gWindow = SDL_CreateWindow( "Legend of Seeplusia!!", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+// 		if( gWindow == NULL )
+// 		{
+// 			printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
+// 			success = false;
+// 		}
+// 		else
+// 		{
+// 			//Create renderer for window
+// 			gRenderer = SDL_CreateRenderer( gWindow, -1, SDL_RENDERER_ACCELERATED );
+// 			if( gRenderer == NULL )
+// 			{
+// 				printf( "Renderer could not be created! SDL Error: %s\n", SDL_GetError() );
+// 				success = false;
+// 			}
+// 			else
+// 			{
+// 				//Initialize renderer color
+// 				SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
 
-				//Initialize PNG loading
-				int imgFlags = IMG_INIT_PNG;
-				if( !( IMG_Init( imgFlags ) & imgFlags ) )
-				{
-					printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() );
-					success = false;
-				}
-				// if( Mix_OpenAudio( 44100, MIX_DEFAULT_FORMAT, 2, 2048 ) < 0 )
-				// {
-				// 	printf( "SDL_mixer could not initialize! SDL_mixer Error: %s\n", Mix_GetError() );
-				// 	success = false;
-				// }
-			}
-		}
-	}
+// 				//Initialize PNG loading
+// 				int imgFlags = IMG_INIT_PNG;
+// 				if( !( IMG_Init( imgFlags ) & imgFlags ) )
+// 				{
+// 					printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() );
+// 					success = false;
+// 				}
+// 				// if( Mix_OpenAudio( 44100, MIX_DEFAULT_FORMAT, 2, 2048 ) < 0 )
+// 				// {
+// 				// 	printf( "SDL_mixer could not initialize! SDL_mixer Error: %s\n", Mix_GetError() );
+// 				// 	success = false;
+// 				// }
+// 			}
+// 		}
+// 	}
 
-	return success;
-}
-void close()
-{
-	//Free loaded images
-	SDL_DestroyTexture(assets);
-	assets=NULL;
+// 	return success;
+// }
+// void close()
+// {
+// 	//Free loaded images
+// 	SDL_DestroyTexture(assets);
+// 	assets=NULL;
 	
-	//Destroy window
-	SDL_DestroyRenderer( gRenderer );
-	SDL_DestroyWindow( gWindow );
-	gWindow = NULL;
-	gRenderer = NULL;
-	// Mix_FreeMusic(bgMusic);
-	// bgMusic = NULL;
-	//Quit SDL subsystems
-	IMG_Quit();
-	// Mix_Quit();
-	SDL_Quit();
-}
-bool loadMedia()
-{
-	//Loading success flag
-	bool success = true;
+// 	//Destroy window
+// 	SDL_DestroyRenderer( gRenderer );
+// 	SDL_DestroyWindow( gWindow );
+// 	gWindow = NULL;
+// 	gRenderer = NULL;
+// 	// Mix_FreeMusic(bgMusic);
+// 	// bgMusic = NULL;
+// 	//Quit SDL subsystems
+// 	IMG_Quit();
+// 	// Mix_Quit();
+// 	SDL_Quit();
+// }
+// bool loadMedia()
+// {
+// 	//Loading success flag
+// 	bool success = true;
 
-	assets = loadTexture("assests.png");
-    if(assets==NULL)
-    {
-        printf("Unable to run due to error: %s\n",SDL_GetError());
-        success =false;
-    }
-	// bgMusic = Mix_LoadMUS( "beat.wav" );
+// 	assets = loadTexture("assests.png");
+//     if(assets==NULL)
+//     {
+//         printf("Unable to run due to error: %s\n",SDL_GetError());
+//         success =false;
+//     }
+// 	// bgMusic = Mix_LoadMUS( "beat.wav" );
 
-	// if(bgMusic == NULL){
-	// 	printf("Unable to load music: %s \n", Mix_GetError());
-	// 	success = false;
-	// }
-	return success;
-}
+// 	// if(bgMusic == NULL){
+// 	// 	printf("Unable to load music: %s \n", Mix_GetError());
+// 	// 	success = false;
+// 	// }
+// 	return success;
+// }
 
 // levels level_change(int coins, performance p)
 // {                                            // this gives us the level
@@ -145,9 +145,9 @@ bool loadMedia()
     return gamestate;
 } */
 
-int main()
-{
-    cout << "this is our project's main file" << endl;
+// int main()
+// {
+//     cout << "this is our project's main file" << endl;
 
-    return 0;
-}
+//     return 0;
+// }
---
+++ window.cpp
@@ -0,0 +1,40 @@
+#include <SDL.h>
+// #include <SDL_image.h>
+// #include <stdio.h>
+#include <iostream>
+// #include <string>
+// #include <stdlib.h>
+// #include <time.h>
+
+int main()
+{
+    if(SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        std::cout << "Failed to initialize the SDL2 library\n";
+        return -1;
+    }
+
+    SDL_Window *window = SDL_CreateWindow("SDL2 Window",
+                                          SDL_WINDOWPOS_CENTERED,
+                                          SDL_WINDOWPOS_CENTERED,
+                                          680, 480,
+                                          0);
+
+    if(!window)
+    {
+        std::cout << "Failed to create window\n";
+        return -1;
+    }
+
+    SDL_Surface *window_surface = SDL_GetWindowSurface(window);
+
+    if(!window_surface)
+    {
+        std::cout << "Failed to get the surface from the window\n";
+        return -1;
+    }
+
+    SDL_UpdateWindowSurface(window);
+
+    SDL_Delay(5000);
+}
