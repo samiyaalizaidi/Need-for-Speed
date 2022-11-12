83bf761c74a9f8ecc3873ec94571e1dcce022091
Samiya Ali Zaidi <sa07171@st.habib.edu.pk>
2022-11-12 21:25:55 +0500

    edited main file to load SDL window

--- main.cpp
+++ main.cpp
@@ -1,111 +1,32 @@
-#include <SDL.h>
-#include <SDL_image.h>
-// #include <SDL_mixer.h>
-#include <stdio.h>
-#include "performance.hpp"
-#include "levels.hpp"
+/*
+This is our game's main file. 
+*/
 
+// #include "performance.hpp"
+// #include "levels.hpp"
+#include "game.hpp"
 
-// //Screen dimension constants
-// const int SCREEN_WIDTH = 800;
-// const int SCREEN_HEIGHT = 600;
-// // SDL Functions from hw1
-
-bool init()
-{
-	//Initialization flag
-	bool success = true;
-
-	//Initialize SDL
-	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
-	{
-		printf( "SDL could not initialize! SDL Error: %s\n", SDL_GetError() );
-		success = false;
+// FROM CS/CE 224/272 HOMEWORK 4
+int main(int argc, char *argv[]){
+    Game game;
+    srand(time(NULL));
+    if( !game.init() ){
+		printf( "Failed to initialize!\n" );
+        return 0;
 	}
-	else
-	{
-		//Set texture filtering to linear
-		if( !SDL_SetHint( SDL_HINT_RENDER_SCALE_QUALITY, "1" ) )
-		{
-			printf( "Warning: Linear texture filtering not enabled!" );
-		}
-
-		//Create window
-		gWindow = SDL_CreateWindow( "Legend of Seeplusia!!", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-		if( gWindow == NULL )
-		{
-			printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
-			success = false;
-		}
-		else
-		{
-			//Create renderer for window
-			gRenderer = SDL_CreateRenderer( gWindow, -1, SDL_RENDERER_ACCELERATED );
-			if( gRenderer == NULL )
-			{
-				printf( "Renderer could not be created! SDL Error: %s\n", SDL_GetError() );
-				success = false;
-			}
-			else
-			{
-				//Initialize renderer color
-				SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
-
-				//Initialize PNG loading
-				int imgFlags = IMG_INIT_PNG;
-				if( !( IMG_Init( imgFlags ) & imgFlags ) )
-				{
-					printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() );
-					success = false;
-				}
-				// if( Mix_OpenAudio( 44100, MIX_DEFAULT_FORMAT, 2, 2048 ) < 0 )
-				// {
-				// 	printf( "SDL_mixer could not initialize! SDL_mixer Error: %s\n", Mix_GetError() );
-				// 	success = false;
-				// }
-			}
-		}
-	}
-
-	return success;
-}
-void close()
-{
-	//Free loaded images
-	SDL_DestroyTexture(assets);
-	assets=NULL;
-	
-	//Destroy window
-	SDL_DestroyRenderer( gRenderer );
-	SDL_DestroyWindow( gWindow );
-	gWindow = NULL;
-	gRenderer = NULL;
-	// Mix_FreeMusic(bgMusic);
-	// bgMusic = NULL;
-	//Quit SDL subsystems
-	IMG_Quit();
-	// Mix_Quit();
-	SDL_Quit();
-}
-bool loadMedia()
-{
-	//Loading success flag
-	bool success = true;
-
-	assets = loadTexture("assests.png");
-    if(assets==NULL)
-    {
-        printf("Unable to run due to error: %s\n",SDL_GetError());
-        success =false;
+		//Load media
+    if( !game.loadMedia() ){
+        printf( "Failed to load media!\n" );
+        return 0;
     }
-	// bgMusic = Mix_LoadMUS( "beat.wav" );
 
-	// if(bgMusic == NULL){
-	// 	printf("Unable to load music: %s \n", Mix_GetError());
-	// 	success = false;
-	// }
-	return success;
+    game.run();
+    game.close();
+
+    return 0;
 }
+
+// TODO: HAVE TO FIX THESE FUNCTIONS.
 
 // levels level_change(int coins, performance p)
 // {                                            // this gives us the level
@@ -136,6 +57,7 @@ bool loadMedia()
 //         {gamestate = "Game Won"; } //no coins condition for level 2
 //     }
 // }
+
 /* string state(Time t, int coins, performance p){
     string gamestate;
     if((t.minutes == 3) && (coins >= 10) && (p.getlife() >= 1))
@@ -144,10 +66,3 @@ bool loadMedia()
         gamestate = "Game lost";
     return gamestate;
 } */
-
-int main()
-{
-    cout << "this is our project's main file" << endl;
-
-    return 0;
-}
