d849e3c755a661631ffb63dadbb7f74a893a1c41
Samiya Ali Zaidi <sa07171@st.habib.edu.pk>
2022-11-12 22:25:10 +0500

    created a display class, same as humania in hw4

---
+++ display.cpp
@@ -0,0 +1,5 @@
+#include "display.hpp"
+
+void Display::drawObject(){
+    ship.draw();
+}
---
+++ display.hpp
@@ -0,0 +1,7 @@
+#include "spaceship.hpp"
+
+class Display{
+    spaceship ship;
+    public:
+        void drawObject();
+};
