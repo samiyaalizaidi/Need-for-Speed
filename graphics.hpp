580523cc9146d560d8c024ee38301ac94b7d06ee
Samiya Ali Zaidi <sa07171@st.habib.edu.pk>
2022-11-17 14:14:52 +0500

    graphics hpp; equivalent of game

---
+++ graphics.hpp
@@ -0,0 +1,36 @@
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#include <iostream>
+using namespace std;
+
+// file with all the SDL stuff
+
+class Graphics{
+    public:
+
+        const int SCREEN_WIDTH = 800;
+        const int SCREEN_HEIGHT = 600;
+        const char* WINDOW_TITLE = "Need for Speed";
+
+    private:
+        static Graphics* sInstance;
+        static bool sInitialized;
+        SDL_Window* window;
+        SDL_Renderer* renderer;
+
+    public:
+        static Graphics* Instance();
+        static void Release();
+        static bool Initialized();
+        SDL_Texture* LoadTexture(string path);
+        SDL_Texture* CreateTextTexture(TTF_Font* font, string text, SDL_Color color);
+        void ClearBackBuffer();
+        void DrawTexture(SDL_Texture* tex, SDL_Rect* clip = NULL, SDL_Rect* rend = NULL, float angle = 0.0f, SDL_RendererFlip flip = SDL_FLIP_NONE);
+        void Render();
+
+    private:
+        Graphics();
+        ~Graphics();
+        bool Init();
+};
